4cd90460ab9ef9ea0e6f50e6f5fa32b185859efb
tako2039 <takeo2039@gmail.com>
2022-03-31 23:40:50 -0500

    add file

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int myValue = 1_000;
+    int myMinIntValue = Integer.MIN_VALUE;
+    int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer minimum value: " + myMinIntValue);
+        System.out.println("Integer maximum value: " + myMaxIntValue);
+        System.out.println("Busted max value: " + (myMaxIntValue + 1));
+        System.out.println("Busted min value: " + (myMinIntValue - 1));
+    int myMaxIntTest = 2_147_483_647;
+
+    byte
+    }
+}
